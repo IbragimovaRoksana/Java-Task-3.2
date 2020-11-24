2e9890492b226877a9bb274b2a1bf5a42661e844
IbragimovaRoksana <roksalana91@mail.ru>
2020-11-24 21:41:01 +0500

    Changed programm's text

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         long client_account = 100;
-        long Increase_summ = 1231;
-        long Bonus;
-        if (Increase_summ>1000) {Bonus=Increase_summ/100;}
-            else {Bonus=0;}
-        client_account+=Increase_summ+Bonus;
-        System.out.println("Бонус пополнения счета = " + Bonus);
+        long increase_summ = 1200;
+        long bonus;
+        if (increase_summ>1000) {bonus=increase_summ/100;}
+            else {bonus=0;}
+        client_account=client_account+increase_summ+bonus;
+        System.out.println("Бонус пополнения счета = " + bonus);
         System.out.println("Итоговая сумма на счету клиента = " + client_account);
 }
 }
