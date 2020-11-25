a4e1afac83111dce731017d58cd3fff9426f50da
IbragimovaRoksana <roksalana91@mail.ru>
2020-11-25 15:53:53 +0500

    Changed variable names

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        long client_account = 100;
-        long increase_summ = 1200;
+        long clientAccount = 100;
+        long increaseSumm = 1200;
         long bonus;
-        if (increase_summ>1000) {bonus=increase_summ/100;}
-            else {bonus=0;}
-        client_account=client_account+increase_summ+bonus;
+        if (increaseSumm > 1000) {
+            bonus = increaseSumm / 100;
+        } else {
+            bonus = 0;
+        }
+        clientAccount = clientAccount + increaseSumm + bonus;
         System.out.println("Бонус пополнения счета = " + bonus);
-        System.out.println("Итоговая сумма на счету клиента = " + client_account);
-}
+        System.out.println("Итоговая сумма на счету клиента = " + clientAccount);
+    }
 }
